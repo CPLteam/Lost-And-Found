9cf4b4aa85eb6b3c325a5f84e9827ae3dc58652f
dheanrp <neadhisa@gmail.com>
2019-12-19 15:49:42 +0700

    dhean edit pengambilan

--- application/controllers/Pengambilan.php
+++ application/controllers/Pengambilan.php
@@ -74,9 +74,8 @@ class Pengambilan extends CI_Controller
 			'id_ambil' => uniqid(),
 			'no_laporan' => $this->input->post('no_laporan', true),
 			'nama_pengambil' => $this->input->post('nama_pengambil', true),
-			'no_hp' => $this->input->post('no_hp', true)
-			//'foto_pengambil' => $this->input->post('foto_pengambil', true),
-			//'tgl_pengambilan' => $this->input->post('tgl_pengambilan', true)
+			'no_hp' => $this->input->post('no_hp', true),
+			'tgl_pengambilan' => $this->input->post('tgl_pengambilan', true)
 		);
 
 		$this->pengambilan_model->input_data($data);
