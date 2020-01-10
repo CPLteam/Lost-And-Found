57e1c31dde599eadd8038e3e30f8aba2ca543acd
Galihadgn <gadiguna@gmail.com>
2020-01-10 08:46:29 +0700

    .

--- application/models/Pengambilan_model.php
+++ application/models/Pengambilan_model.php
@@ -18,16 +18,4 @@ class Pengambilan_model extends CI_Model
 	{
 		$this->db->insert('pengambilan', $data);
 	}
-
- 	function get_laporan()
-	{
-		$results = $this->db->select('no_laporan, nama_barang')->get('temuan')->result_array();
-
-		$laporan = array();
-		foreach($results as $result){
-			$laporan[$result['no_laporan']] = $result['nama_barang'];
-		}
-		$laporan[''] = 'Pilih No Laporan'
-		return $laporan;
-	}
 }
